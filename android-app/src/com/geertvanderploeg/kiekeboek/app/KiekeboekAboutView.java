a60805e43c8d0baffc2251c05aa491ea3f0db779
Geert van der Ploeg <geertpl@gmail.com>
2012-06-21 08:38:45 +0200

    Display app version in about-screen

--- android-app/src/com/geertvanderploeg/kiekeboek/app/KiekeboekAboutView.java
+++ android-app/src/com/geertvanderploeg/kiekeboek/app/KiekeboekAboutView.java
@@ -1,17 +1,25 @@
 package com.geertvanderploeg.kiekeboek.app;
 
+import com.geertvanderploeg.kiekeboek.R;
+
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-import com.geertvanderploeg.kiekeboek.R;
+import android.widget.TextView;
 
 public class KiekeboekAboutView extends Activity {
 
-    private static final String TAG = "KiekeboekAboutView";
+  private static final String TAG = "KiekeboekAboutView";
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.v(TAG, "oncreate");
-        setContentView(R.layout.kiekeboek_about);
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.v(TAG, "oncreate");
+    setContentView(R.layout.kiekeboek_about);
+    try {
+      final String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+      ((TextView) findViewById(R.id.aboutversion)).setText("Applicatieversie: " + versionName);
+    } catch (PackageManager.NameNotFoundException ignored) {
     }
+  }
 }
