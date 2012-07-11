2c24455d4c3b05d446833739f93780759f98d02a
Geert van der Ploeg <geertpl@gmail.com>
2012-07-11 14:28:15 +0200

    #1: Do not add phone numbers for children

--- android-app-test/src/com/geertvanderploeg/kiekeboek/platform/ContactManagerTest.java
+++ android-app-test/src/com/geertvanderploeg/kiekeboek/platform/ContactManagerTest.java
@@ -1,20 +1,30 @@
 package com.geertvanderploeg.kiekeboek.platform;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.geertvanderploeg.kiekeboek.KiekeboekContentProvider;
 import com.geertvanderploeg.kiekeboek.client.User;
 
 import android.test.ProviderTestCase2;
 
-public class ContactManagerTest extends ProviderTestCase2<Contacts> {
+public class ContactManagerTest extends ProviderTestCase2<KiekeboekContentProvider> {
 
 
   public ContactManagerTest() {
-    super(KiekeboekContentProvider.class, "foo");
+    super(KiekeboekContentProvider.class, KiekeboekContentProvider.class.getName());
   }
 
   public void testSyncContacts() {
-    ContactManager.syncContacts(getMockContext(), "foo", new ArrayList<User>());
+    final ArrayList<User> users = new ArrayList<User>();
+    users.add(buildUser(1));
+    users.add(buildUser(2));
+    users.add(buildUser(3));
+    ContactManager.syncContacts(getMockContext(), "foo", users);
+  }
+
+  private User buildUser(int seed) {
+    return new User("name" + seed, "firstname"+seed, "middlename"+seed, "lastername"+seed, "street", "postcode",
+        "city", "0612123123", "0725123123", "0235123123", "email@email.com", new Date(), "W3", "3a", false, seed);
   }
 }
